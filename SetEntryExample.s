c5503ee64fa9e347718660f0fe3934c2e9656a59
Renfong <windless99@gmail.com>
2021-09-06 22:26:15 +0800

    Add files via upload

---
+++ SetEntryExample.s
@@ -0,0 +1,43 @@
+// An Example to show how to set entry content in a dialog
+//
+// Renfong
+// 2021/09/06
+
+class TestUI : UIFrame	{
+	number true, false, ver
+	TagGroup EntryText
+	
+	TestUI(object self){
+		true=1
+		false=0
+		ver=1.0
+		result("TestUI ["+self.ScriptObjectGetID()+"] constructed")
+	};
+	
+	~TestUI(object self){
+		result("TestUI ["+self.ScriptObjectGetID()+"] destructed")
+	};
+	
+	void GetImgName(object self)	{
+		image src := GetFrontImage()
+		EntryText.DLGValue(src.GetName())
+	};
+	
+	void CreateDialog(object self)	{
+		String UIName = "TestUI"
+		TagGroup MainUI = DLGCreateDialog(UIName)
+		
+		TagGroup tgBox = DLGCreateBox("Func")
+		TagGroup tgEntry = DLGCreateStringField("GetName :", EntryText, "?", 20)
+		TagGroup tgBtn = DLGCreatePushButton("GetFrontImage()","GetImgName")
+		tgBox.DLGAddElement(tgEntry)
+		tgBox.DLGAddElement(tgBtn)
+		
+		MainUI.DLGAddElement(tgBox)
+		self.init(MainUI).Display(UIName+"_v"+ver.format("%.1f"))
+	};
+};
+
+{
+	Alloc(TestUI).CreateDialog()
+}
